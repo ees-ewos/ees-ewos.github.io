21e7c999dfc74292b0a8199f41b529bc14a8f63b
Zoie <zoiezoie07@gmail.com>
2023-12-20 16:25:59 +0000

    update

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # Project Z
 
 ## to-do list
+- fix hamburger covering h1
 - sections
+- coming up with what to write
+
+## google fonts
+- https://fonts.google.com/share?selection.family=M+PLUS+Code+Latin:wdth,wght@100,100;105,200;125,100;125,300
--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 function toggleBurger() {
     document.querySelector(".hamburger-ul").classList.toggle("burger");
 }
+
+window.onscroll = function() {
+    if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
+        document.getElementById("to-top").style.display = "block";
+    } else {
+        document.getElementById("to-top").style.display = "none";
+    }
+};
+
+function toTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
