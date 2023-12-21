a24ead4337af9192f2b139d1d86b4e9b08dfc80f
Zoie <zoiezoie07@gmail.com>
2023-12-21 00:40:59 +0000

    minor

--- script.js
+++ script.js
@@ -11,6 +11,5 @@ window.onscroll = function() {
 };
 
 function toTop() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
+    window.scrollTo({top: 0, behavior: 'smooth'});
 }
